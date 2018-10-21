23aac02c5b4b2fbc3f7517eb33dffb691c8aaa37
Lotushighwave <lotus.highwave@gmail.com>
2018-10-21 15:29:11 +0900

    initialize

---
+++ anxiety-diary/src/main/java/com/anxietydiary/AnxietyDiaryApplication.java
@@ -0,0 +1,12 @@
+package com.anxietydiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnxietyDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnxietyDiaryApplication.class, args);
+	}
+}
